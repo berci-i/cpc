9f352c7a9dc107cad4a4d68bb52a0121d40c3d5e
Berci <bercean.ioan@gmail.com>
2024-05-12 18:09:07 +0300

    Add three. Add scene and custom hook to load it. Render random box

---
+++ src/components/Home/ThreeScene.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import useScene from "./threeScene/useScene";
+
+const ThreeScene: React.FC = () => {
+  const containerRef = useScene()
+  
+  return <div ref={containerRef} />;
+};
+export default ThreeScene;
---
+++ src/pages/index.tsx
@@ -0,0 +1,25 @@
+import Head from "next/head";
+import { Inter } from "next/font/google";
+import ThreeScene from "@/components/Home/ThreeScene";
+import styles from "@/styles/Home.module.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Label cuboids in a point cloud</title>
+        <meta name="description" content="Simple front-end web application to create/adjust cuboids" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={`${styles.main} ${inter.className}`}>
+        <div className={styles.description}>
+          <ThreeScene />
+        </div>
+
+      </main>
+    </>
+  );
+}
